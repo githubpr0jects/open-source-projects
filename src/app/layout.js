529687e912ac97ebc27c0f13fb20e08d76205a9e
M Shehzen <mshehzen@gmail.com>
2026-09-13 19:37:47 +0530

    Minor - GA update

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,8 @@ export const metadata = {
   authors: [{ name: 'Open-source Projects Team' }],
   creator: 'Open-source Projects',
   publisher: 'Open-source Projects',
-  
+
+
   // Open Graph metadata
   openGraph: {
     type: 'website',
@@ -43,7 +44,7 @@ export const metadata = {
       },
     ],
   },
-  
+
   // Twitter Card metadata
   twitter: {
     card: 'summary_large_image',
@@ -53,7 +54,7 @@ export const metadata = {
     creator: '@opensourceprojects',
     site: '@opensourceprojects',
   },
-  
+
   // Additional metadata
   robots: {
     index: true,
@@ -66,12 +67,12 @@ export const metadata = {
       'max-snippet': -1,
     },
   },
-  
+
   // App metadata
   applicationName: 'Open-source Projects',
   generator: 'Next.js',
   referrer: 'origin-when-cross-origin',
-  
+
   // Language and locale
   metadataBase: new URL('https://www.opensourceprojects.dev'),
 }
@@ -147,34 +148,36 @@ export default function RootLayout({ children }) {
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
         />
         {/* RSS Feed Discovery */}
-        <link 
-          rel="alternate" 
-          type="application/rss+xml" 
-          title="Open Source Projects RSS Feed" 
-          href="https://www.opensourceprojects.dev/rss" 
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="Open Source Projects RSS Feed"
+          href="https://www.opensourceprojects.dev/rss"
         />
-        <link 
-          rel="alternate" 
-          type="application/atom+xml" 
-          title="Open Source Projects Atom Feed" 
-          href="https://www.opensourceprojects.dev/feed.xml" 
+        <link
+          rel="alternate"
+          type="application/atom+xml"
+          title="Open Source Projects Atom Feed"
+          href="https://www.opensourceprojects.dev/feed.xml"
         />
-        
+
         {/* Font Awesome - Complete Kit */}
-        <link 
-          rel="stylesheet" 
+        <link
+          rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
           integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
-        <link 
-          rel="stylesheet" 
+        <link
+          rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/brands.min.css"
           integrity="sha512-8RxmFOVaKQe/xtg6lbscU9DU0IRhURWEuiI0tXevv+lXbAHfkpamD4VKFQRto9WgfOJDwOZ74c/s9Yesv3VvIQ=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
+        {/*Gitads verification */ }
+        <meta name="gitads-verification" content="7D726E8F76D4029A46CF2617932FCBD4" />
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-PM4NQK4MYG"
